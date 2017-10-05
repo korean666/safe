a722dc3501fcc2d2302f63a64037b9046da0fdfd
korean666 <32029848+korean666@users.noreply.github.com>
2017-10-06 03:21:09 +0700

    Add files via upload

--- pemanggil.js
+++ pemanggil.js
@@ -19,12 +19,12 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/10/now-you-can-have-great-car-insurance.html"}
-if(x=="2"){xxx="/2017/10/online-car-insurance-quotes-it-offers.html"}
-if(x=="3"){xxx="/2017/10/cheap-car-insurance-quotes-online-can.html"}
-if(x=="4"){xxx="/2017/10/everything-to-know-about-auto-insurance.html"}a_to_ve[i].href="http://audiophileroomsafe.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="2017/10/now-you-can-have-great-car-insurance.html"}
+if(x=="2"){xxx="2017/10/online-car-insurance-quotes-it-offers.html"}
+if(x=="3"){xxx="2017/10/cheap-car-insurance-quotes-online-can.html"}
+if(x=="4"){xxx="2017/10/everything-to-know-about-auto-insurance.html"}a_to_ve[i].href="http://audiophileroomsafe.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
